f7cb79c3f0564dced7489dec70dfa72f729b0f63
deepakcode21@gmail.com <deepakcode21@gmail.com>
2025-01-30 21:47:03 +0530

    route setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Professional from './pages/Professional'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
 
 const App = () => {
   return (
-    <div>
+    <div className='mx-4 sm:mx-[10%]'>
 
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/my-profile' element={<MyProfile/>} />
+        <Route path='/professional' element={<Professional/>} />
+        <Route path='/my-appointments' element={<MyAppointments/>} />
+        <Route path='/appointment/:proId' element={<Appointment/>} />
+        <Route path='/professional/:services' element={<Professional/>} />
+      </Routes>
+
     </div>
   )
 }
