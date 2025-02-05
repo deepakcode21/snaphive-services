07f7b514d386ef8d3ba711bf873a72e5387334d7
deepakcode21@gmail.com <deepakcode21@gmail.com>
2025-02-06 01:56:47 +0530

    ui enhance testimonial

--- frontend/src/components/Testimonial.jsx
+++ frontend/src/components/Testimonial.jsx
@@ -160,7 +160,7 @@ const Testimonial = () => {
                 }}
                 className={`w-2 h-2 rounded-full transition-colors duration-300 ${
                   index === activeTestimonial
-                    ? 'bg-blue-500'
+                    ? 'bg-black'
                     : 'bg-gray-300 hover:bg-gray-400'
                 }`}
               />
