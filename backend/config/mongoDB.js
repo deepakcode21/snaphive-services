a62c7f858c3b13ccac5647d5404be4a4082bb930
deepakcode21@gmail.com <deepakcode21@gmail.com>
2025-02-13 20:32:18 +0530

    connect databases

---
+++ backend/config/mongoDB.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+
+    mongoose.connection.on('connected', () => console.log("Datebase Connected"))
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/snaphive`)
+}
+
+export default connectDB
---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongoDB.js';
+import connectCloudinary from './config/cloudinary.js';
+
+
+// app config 
+const app = express()
+const prot = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// api endpoints
+app.get('/', (req, res)=>{
+    res.send('API WORKING hello word')
+})
+
+app.listen(prot, ()=> console.log("Server started", prot))
